f9f0d0a36472c9693271b400a9166509633c1716
tttuuu123 <417632455@qq.com>
2020-06-20 21:24:31 +0800

    2020.6.20

---
+++ leetcode/easy/最大子序和.js
@@ -0,0 +1,29 @@
+
+/* 
+    给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+    示例:
+
+    输入: [-2,1,-3,4,-1,2,1,-5,4],
+    输出: 6
+    解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/maximum-subarray
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let res = nums[0];
+    let temp = 0;
+    nums.forEach((n) => {
+        temp += n;
+        res = Math.max(res, temp);
+        if (temp < 0) temp = 0;
+    });
+    return res;
+};
