5a616a1b3e8973917e4c4ea2b5ff102577f8e3f4
tttuuu123 <417632455@qq.com>
2020-07-19 10:47:42 +0800

    2020.7.19

---
+++ leetcode/normal/删除链表的倒数第N个节点.js
@@ -0,0 +1,64 @@
+/* 
+  给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+  示例：
+
+  给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+  当删除了倒数第二个节点后，链表变为 1->2->3->5.
+  说明：
+
+  给定的 n 保证是有效的。
+
+  进阶：
+
+  你能尝试使用一趟扫描实现吗？
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  let cur = head;
+  const arr = [];
+  while (cur) {
+    arr.push(cur);
+    cur = cur.next;
+  }
+  const tarNodeIdx = arr.length - n;
+  const preNode = arr[tarNodeIdx - 1];
+  if (!preNode) {
+    return head.next;
+  }
+  preNode.next = preNode.next.next;
+  return head;
+};
+
+/* 添加一个空节点，可以避免n与节点长度相同的场景 */
+var removeNthFromEnd = function(head, n) {
+  const pre = new ListNode(null);
+  pre.next = head;
+  let cur = pre;
+  const arr = [];
+  while (cur) {
+    arr.push(cur);
+    cur = cur.next;
+  }
+  const tarNodeIdx = arr.length - n;
+  const preNode = arr[tarNodeIdx - 1];
+  preNode.next = preNode.next.next;
+  return pre.next;
+};
