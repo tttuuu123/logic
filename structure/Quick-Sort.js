285344507f8351af3e2fb9fe33833d893d4687c6
shaojiaqi <shaojiaqi@youzan.com>
2021-05-25 23:50:49 +0800

    2021.5.25

---
+++ structure/Quick-Sort.js
@@ -0,0 +1,31 @@
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+function getMid(a, b, c) {
+  if (a >= b && a <= c) return a;
+  if (b >= a && b <= c) return b;
+  return c;
+}
+
+const threshold = 0;  // 设置分区大小
+
+export function QuickSort(arr, left = 0, right = arr.length - 1) {
+  while (right - left > threshold) {
+    let l = left;
+    let r = right;
+    const base = getMid(arr[l], arr[(l + r) >> 1], arr[r]);
+    while (l <= r) {
+      while (arr[l] < base) l += 1;
+      while (arr[r] > base) r -= 1;
+      if (l <= r) {
+        swap(arr, l, r);
+        l += 1;
+        r -= 1;
+      };
+    }
+    QuickSort(arr, l, right);  // 右侧递归
+    right = r;  // 左循环 减少一半递归调用
+  }
+}
+
