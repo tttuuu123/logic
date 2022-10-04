e35522fb43c6c871bca181e38218f3a0c8e5de06
shaojiaqi <shaojiaqi@youzan.com>
2022-10-04 16:03:01 +0800

    2022.10.4

---
+++ leetcode/easy/剑指 Offer 58 - II. 左旋转字符串.js
@@ -0,0 +1,31 @@
+/* 
+  字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+  示例 1：
+
+  输入: s = "abcdefg", k = 2
+  输出: "cdefgab"
+  示例 2：
+
+  输入: s = "lrloseumgh", k = 6
+  输出: "umghlrlose"
+
+  限制：
+
+  1 <= k < s.length <= 10000
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * @param {string} s
+ * @param {number} n
+ * @return {string}
+ */
+var reverseLeftWords = function(s, n) {
+  const reverse = s.substring(0, n);
+  const remain = s.substring(n);
+  return remain + reverse;
+};
